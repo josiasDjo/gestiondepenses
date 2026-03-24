ff530b397ae4d4471048b5296eb6294732208e2c
josiasDjo <djodev23@gmail.com>
2026-03-24 19:54:06 +0200

    BD config connection & schema

---
+++ backend/config/env.js
@@ -0,0 +1,13 @@
+//Serveur
+PORT=5000
+NODE_ENV=development
+
+//Base de données
+DB_HOST=localhost
+DB_USER=root
+DB_PASSWORD=your_password
+DB_NAME=gestion_depenses
+
+//JWT
+JWT_SECRET=votre_secret_key_ici
+JWT_EXPIRE=7
