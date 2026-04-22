48e6093f634a19698898177e22843931e57adc8b
josiasDjo <djodev23@gmail.com>
2026-04-22 19:53:21 +0100

    Devise check != null

--- backend/routes/invitationRoutes.js
+++ backend/routes/invitationRoutes.js
@@ -4,7 +4,7 @@ const invitationController = require('../controllers/invitationController');
 const { protect } = require('../middlewares/authMiddleware');
 
 // Routes pour les invitations
-router.post('/envoyer', invitationController.envoyerInvitation);
+router.post('/envoyerInvitation', invitationController.envoyerInvitation);
 router.get('/mes-invitations', invitationController.getMesInvitations);
 router.post('/:id/accepter', invitationController.accepterInvitationParId);
 router.post('/:id/refuser', invitationController.refuserInvitation); 
